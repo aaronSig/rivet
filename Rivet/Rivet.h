998400b6233aec464b61e26e7ae10158793debef
Aaron Signorelli <aaron.signorelli@bullorbear.com>
2014-01-22 01:59:00 +0000

    header

--- Rivet/Rivet.h
+++ Rivet/Rivet.h
@@ -6,11 +6,18 @@
 //  Copyright (c) 2014 Five Heads. All rights reserved.
 //
 
-#import "Rivetable.h"
+#import "Binding.h"
 #import "NSObject+BlockObservation.h"
+#import "RVMutableDictionary.h"
+#import "RivetControl.h"
+#import "RivetElement.h"
+#import "RivetView.h"
+#import "UIControl+RivetControl.h"
+#import "UIImageView+RivetView.h"
+#import "UILabel+RivetView.h"
+#import "UISlider+RivetControl.h"
+#import "UITextField+RivetControl.h"
+#import "UIView+HideAndShow.h"
 #import "UIView+Rivet.h"
+#import "UIView+template.h"
 #import "UIViewController+Rivet.h"
-#import "UILabel+RivetBinder.h"
-#import "UITextField+RivetBinder.h"
-#import "UISlider+RivetBinder.h"
-#import "UIImageView+RivetBinder.h"
